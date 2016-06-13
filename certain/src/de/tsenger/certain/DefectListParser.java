dc1ebcd3a85d2de08791ca98ff215b01fd7cbf32
Tobias Senger <tobias@t-senger.de>
2016-06-13 15:53:55 +0200

    some restructure on defect/deviantion list parsing

--- certain/src/de/tsenger/certain/DefectListParser.java
+++ certain/src/de/tsenger/certain/DefectListParser.java
@@ -1,23 +1,14 @@
 package de.tsenger.certain;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PublicKey;
 import java.security.SignatureException;
-import java.security.cert.CertSelector;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.X509CertSelector;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import javax.security.auth.x500.X500Principal;
 
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1Integer;
@@ -29,14 +20,6 @@ import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.DLSet;
 import org.bouncycastle.asn1.cms.IssuerAndSerialNumber;
 import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.cms.CMSException;
-import org.bouncycastle.cms.CMSProcessableByteArray;
-import org.bouncycastle.cms.CMSSignedData;
-import org.bouncycastle.cms.SignerInformation;
-import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.util.Store;
 
 import de.tsenger.certain.asn1.eac.BSIObjectIdentifiers;
 import de.tsenger.certain.asn1.mrtdpki.Defect;
@@ -44,70 +27,17 @@ import de.tsenger.certain.asn1.mrtdpki.DefectList;
 import de.tsenger.certain.asn1.mrtdpki.KnownDefect;
 import de.tsenger.tools.HexString;
 
-public class DefectListParser {
+public class DefectListParser extends DListParser{
 	
-	private List<Certificate> defectListSignerCertificates;
 	private DefectList defectList;
-	private CMSSignedData cmsSignedData;
-	private SignerInformation signerInfo;
-
-	/** Use this to get all defectListSignerCertificates, including link defectListSignerCertificates. */
-	private static final CertSelector IDENTITY_SELECTOR = new X509CertSelector() {
-		@Override
-		public boolean match(Certificate cert) {
-			if (!(cert instanceof X509Certificate)) { return false; }
-			return true;
-		}
-
-		@Override
-		public Object clone() { return this; }	
-	};
-
-	/** Use this to get self-signed defectListSignerCertificates only. (Excludes link defectListSignerCertificates.) */
-	private static final CertSelector SELF_SIGNED_SELECTOR = new X509CertSelector() {
-		@Override
-		public boolean match(Certificate cert) {
-			if (!(cert instanceof X509Certificate)) { return false; }
-			X509Certificate x509Cert = (X509Certificate)cert;
-			X500Principal issuer = x509Cert.getIssuerX500Principal();
-			X500Principal subject = x509Cert.getSubjectX500Principal();
-			return (issuer == null && subject == null) || subject.equals(issuer);
-		}
-
-		@Override
-		public Object clone() { return this; }
-	};
-	
-
-	/** Private constructor, only used locally. */
-	private DefectListParser() {
-		this.defectListSignerCertificates = new ArrayList<Certificate>(4);	
-	}
-	
-	
-	public DefectListParser(byte[] binary, CertSelector selector) {
-		this();
-		
-		this.cmsSignedData = buildCMSSignedDataFromBinary(binary);		
-		this.signerInfo = parseSignerInfo();
-		this.defectList = parseDefectList();
-		this.defectListSignerCertificates = getDefectListSignerCertificates(cmsSignedData);		
-	}
-	
 
 	public DefectListParser(byte[] binary) {
-		this(binary, IDENTITY_SELECTOR);
+		super(binary);
+		defectList = DefectList.getInstance(super.getdList());
 	}
 	
-	public List<Certificate> getDefectListSignerCertificates() {
-		return defectListSignerCertificates;
-	}
-	
-	public DefectList getDefectList() {
-		return defectList;
-	}
-	
-	public String getDefectListInfoString(boolean showDetails) {
+	@Override
+	public String getDListInfoString(boolean showDetails) {
 		
 		Defect defect;
 		
@@ -116,11 +46,11 @@ public class DefectListParser {
 		if 	(cmsSignedData.getVersion()!=3)	System.out.println("SignedData Version SHOULD be 3 but is "+ cmsSignedData.getVersion()+"!");
 		
 		sw.write("\nThis Deviation List contains defects from "+defectList.getDefects().size()+" different DS certificates\n");
-		sw.write("and contains "+defectListSignerCertificates.size()+" Defects List Signer certificates:\n\n");
+		sw.write("and contains "+dListSignerCertificates.size()+" Defects List Signer certificates:\n\n");
 		
 		PublicKey pubKey = null;	
 		
-		for (Certificate cert : defectListSignerCertificates) {			
+		for (Certificate cert : dListSignerCertificates) {			
 			X509Certificate x509Cert = (X509Certificate) cert;
 			
 			if (x509Cert.getSubjectDN().toString().equals(x509Cert.getIssuerDN().toString())) {
@@ -130,7 +60,7 @@ public class DefectListParser {
 		
 		if (pubKey != null) {
 
-			for (Certificate cert : defectListSignerCertificates) {
+			for (Certificate cert : dListSignerCertificates) {
 				
 				X509Certificate x509Cert = (X509Certificate) cert;
 
@@ -265,90 +195,6 @@ public class DefectListParser {
 		}
 
 		return sw.toString();
-	}
-	
-	public boolean verifySignedData() {
-		boolean result = false;
-		for (Certificate cert : defectListSignerCertificates) {			
-			X509Certificate x509Cert = (X509Certificate) cert;
-			
-			if (x509Cert.getSubjectDN().toString().equals(x509Cert.getIssuerDN().toString())) {
-				SignedDataVerifier verifier = new SignedDataVerifier(x509Cert);
-				try {
-					result = verifier.signatureVerified(cmsSignedData);
-				} catch (CertificateException | OperatorCreationException | CMSException e) {
-					System.out.println("Couldn't verify signature of SignedData objekt: "+e.getMessage());
-				}				
-			}				
-		}
-		return result;
-	}
-	
-	/* PRIVATE METHODS BELOW */
-	
-	private SignerInformation parseSignerInfo() {
-		
-		Iterator<SignerInformation> iterator = cmsSignedData.getSignerInfos().getSigners().iterator();
-
-		this.signerInfo = iterator.next();
-		return signerInfo;
-	}
-	
-	private DefectList parseDefectList() {
-		
-		DefectList defectList = null;
-
-		String id_DefectList = cmsSignedData.getSignedContentTypeOID(); 
-		CMSProcessableByteArray content = (CMSProcessableByteArray) cmsSignedData.getSignedContent();
-		
-		if (id_DefectList.equals(BSIObjectIdentifiers.DefectList.toString())) {
-			ByteArrayOutputStream bout = new ByteArrayOutputStream();
-			
-			try {
-				content.write(bout);
-			} catch (IOException | CMSException e) {
-				System.out.println(e.getLocalizedMessage());
-			}
-			
-			byte[] octets = bout.toByteArray();
-			defectList = DefectList.getInstance(octets);
-		}
-		return defectList;
-	}
-
-	
-	private List<Certificate> getDefectListSignerCertificates(CMSSignedData signedData) {
-		
-		List<Certificate> result = new ArrayList<Certificate>();
-
-		// The signer certifcate(s)
-		Store certStore = signedData.getCertificates();
-		
-		JcaX509CertificateConverter converter = new JcaX509CertificateConverter();
-		converter.setProvider("BC");
-		
-		ArrayList<X509CertificateHolder> certificateHolders = (ArrayList<X509CertificateHolder>)certStore.getMatches(null); 
-
-		 for(X509CertificateHolder holder: certificateHolders){
-			try {
-				X509Certificate cert = converter.getCertificate(holder);
-				result.add(cert);
-			} catch (CertificateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		 }
-		return result;
-	}
-	
-	private CMSSignedData buildCMSSignedDataFromBinary(byte[] binary) {
-		CMSSignedData signedData =null;
-		try {
-			signedData = new CMSSignedData(binary);
-		} catch (CMSException e) {
-			System.out.println("Could find a SignedData object: "+e.getLocalizedMessage());
-		}
-		return signedData;
 	}
 	
 }
