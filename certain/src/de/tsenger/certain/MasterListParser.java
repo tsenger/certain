b0bbde46b2ebbca989d4dd2a4385863c76e8c12f
Tobias Senger <tobias@t-senger.de>
2021-08-17 12:08:55 +0200

    Output ML signer certificates (issue #3), output all certificate details if details switch was set

--- certain/src/de/tsenger/certain/MasterListParser.java
+++ certain/src/de/tsenger/certain/MasterListParser.java
@@ -1,7 +1,9 @@
 package de.tsenger.certain;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -10,6 +12,7 @@ import java.security.SignatureException;
 import java.security.cert.CertSelector;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509CertSelector;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
@@ -101,12 +104,31 @@ public class MasterListParser {
 	}
 	
 	public String getMasterListInfoString(boolean showDetails) {
-		
+			
 		StringWriter sw = new StringWriter();
 		
 		int i=0;
 		
+		
 		sw.append("\nThis Master List contains "+cscaCerts.size()+" CSCA certificates. \nand "+masterListSignerCertificates.size()+" Master List Signer Certificates.");
+		
+		for (Certificate mlSigner : masterListSignerCertificates) {
+			sw.append("+++++++++++++ Masterlist Signer Cert no. "+(++i)+" ++++++++++++++\n");
+			
+			X509Certificate x509Cert = (X509Certificate) mlSigner;
+
+			String subjectDN = x509Cert.getSubjectDN().toString();
+			String issuerDN = x509Cert.getIssuerDN().toString();
+			
+			sw.write("Subject DN: "+subjectDN+"\n");
+			sw.write("Issuer  DN:  "+issuerDN+"\n");
+			DEROctetString oct = (DEROctetString) DEROctetString.getInstance(x509Cert.getExtensionValue("2.5.29.14"));
+			SubjectKeyIdentifier skid = SubjectKeyIdentifier.getInstance(oct.getOctets());
+			sw.write("X509 SubjectKeyIdentifier: "+HexString.bufferToHex(skid.getKeyIdentifier())+"\n");
+		}
+		
+		sw.append("\n");		
+		i = 0;
 		
 		for (Certificate cert : cscaCerts) {
 			sw.append("+++++++++++++ CSCA Cert no. "+(++i)+" ++++++++++++++\n");
@@ -120,16 +142,19 @@ public class MasterListParser {
 			sw.write("Issuer  DN:  "+issuerDN+"\n");
 			DEROctetString oct = (DEROctetString) DEROctetString.getInstance(x509Cert.getExtensionValue("2.5.29.14"));
 			SubjectKeyIdentifier skid = SubjectKeyIdentifier.getInstance(oct.getOctets());
-			sw.write("X509 SubjectKeyIdentifier: "+HexString.bufferToHex(skid.getKeyIdentifier())+"\n");
+			sw.write("X509 SubjectKeyIdentifier: "+HexString.bufferToHex(skid.getKeyIdentifier()) + "\n");
+			sw.append("Public Key Algorithm: " + x509Cert.getPublicKey().getAlgorithm() + "\n");
+			sw.append("Signing Algorithm: " + x509Cert.getSigAlgName() + "\n");
+			if (showDetails) sw.append(x509Cert.toString());
 			
-			try {
-				cert.verify(cert.getPublicKey());
-				sw.append("Signature is valid\n");
-
-			} catch (InvalidKeyException | CertificateException | NoSuchAlgorithmException | NoSuchProviderException | SignatureException e) {
-				// TODO Auto-generated catch block
-				sw.append(e.getMessage());
-			} 
+//			try {
+//				cert.verify(x509Cert.getPublicKey());
+//				sw.append("Signature is valid.");
+//
+//			} catch (InvalidKeyException | CertificateException | NoSuchAlgorithmException | NoSuchProviderException | SignatureException e) {
+//				// TODO Auto-generated catch block
+//				sw.append(e.getMessage());
+//			} 
 			
 			sw.append("\n\n");
 		}
@@ -180,6 +205,7 @@ public class MasterListParser {
 
 		try {
 			Enumeration<?> derObjects = ASN1Sequence.getInstance(octets).getObjects();
+			CertificateFactory cf = CertificateFactory.getInstance("X.509","BC");
 			
 			while (derObjects.hasMoreElements()) {
 				ASN1Integer version = (ASN1Integer)derObjects.nextElement(); //Should be 0
@@ -189,7 +215,7 @@ public class MasterListParser {
 				Enumeration<Certificate> certs = certSet.getObjects();				
 				while (certs.hasMoreElements()) {
 					org.bouncycastle.asn1.x509.Certificate certAsASN1Object = org.bouncycastle.asn1.x509.Certificate.getInstance(certs.nextElement());
-					cscaCerts.add(new X509CertificateObject(certAsASN1Object));
+					cscaCerts.add(cf.generateCertificate(new ByteArrayInputStream(certAsASN1Object.getEncoded())));
 				}
 				
 			}
